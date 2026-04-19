9ac3884ac5047cb43cdfe5a1d99d6b476697f6f3
Aaron Jarrette <ajarrette@yahoo.com>
2026-04-18 20:05:23 -0600

    Fixed crash on load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react';
 import { Stack } from 'expo-router';
 import FilmsDbProvider from './components/filmsDbProvider';
-import StartupScreen from './startup';
 
 export default function RootLayout() {
-  const [showStartup, setShowStartup] = useState(true);
-
   return (
     <FilmsDbProvider>
-      {showStartup ? (
-        <StartupScreen onDone={() => setShowStartup(false)} />
-      ) : (
-        <Stack
-          initialRouteName='(tabs)'
-          screenOptions={{
-            headerShown: false,
-            animation: 'none',
-            contentStyle: { backgroundColor: '#25292e' },
+      <Stack
+        initialRouteName='(tabs)'
+        screenOptions={{
+          headerShown: false,
+          animation: 'none',
+          contentStyle: { backgroundColor: '#25292e' },
+        }}
+      >
+        <Stack.Screen name='(tabs)' options={{ animation: 'none' }} />
+        <Stack.Screen
+          name='people'
+          options={{
+            animation: 'slide_from_right',
           }}
-        >
-          <Stack.Screen name='(tabs)' options={{ animation: 'none' }} />
-          <Stack.Screen
-            name='people'
-            options={{
-              animation: 'slide_from_right',
-            }}
-          />
-          <Stack.Screen name='+not-found' options={{ headerShown: true }} />
-        </Stack>
-      )}
+        />
+        <Stack.Screen name='+not-found' options={{ headerShown: true }} />
+      </Stack>
     </FilmsDbProvider>
   );
 }
