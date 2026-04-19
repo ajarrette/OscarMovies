7517be6a34021213464609241472027a56352444
Aaron Jarrette <ajarrette@yahoo.com>
2026-04-18 20:01:12 -0600

    Remove slide in animation on load

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react';
 import { Stack } from 'expo-router';
 import FilmsDbProvider from './components/filmsDbProvider';
+import StartupScreen from './startup';
 
 export default function RootLayout() {
+  const [showStartup, setShowStartup] = useState(true);
+
   return (
     <FilmsDbProvider>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='(tabs)' />
-        <Stack.Screen
-          name='people'
-          options={{
-            animation: 'slide_from_right',
+      {showStartup ? (
+        <StartupScreen onDone={() => setShowStartup(false)} />
+      ) : (
+        <Stack
+          initialRouteName='(tabs)'
+          screenOptions={{
+            headerShown: false,
+            animation: 'none',
+            contentStyle: { backgroundColor: '#25292e' },
           }}
-        />
-        <Stack.Screen name='+not-found' options={{ headerShown: true }} />
-      </Stack>
+        >
+          <Stack.Screen name='(tabs)' options={{ animation: 'none' }} />
+          <Stack.Screen
+            name='people'
+            options={{
+              animation: 'slide_from_right',
+            }}
+          />
+          <Stack.Screen name='+not-found' options={{ headerShown: true }} />
+        </Stack>
+      )}
     </FilmsDbProvider>
   );
 }
